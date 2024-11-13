ccf1470a7624112a62876624a46058ffa4c3396b
Bernardo <bernardpena@gmail.com>
2024-11-13 02:14:50 -0300

    Modificacion de Login mediante Modal

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Login from '../components/Login';
 
 function Navbar() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark ">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-        <img class="logo" src="../public/AromasdeCafe_LOGO.png" alt="Aromas de Café Logo"></img>
+          <img className="logo" src="../public/AromasdeCafe_LOGO.png" alt="Aromas de Café Logo" />
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -16,7 +20,7 @@ function Navbar() {
               <Link className="nav-link" to="/register">Registro</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/login">Inicio de Sesión</Link>
+              <a className="nav-link" href="#" onClick={() => setModalOpen(true)}>Inicio de Sesión</a>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/cart">Carrito</Link>
@@ -24,6 +28,16 @@ function Navbar() {
           </ul>
         </div>
       </div>
+
+
+      {modalOpen && (
+        <div className="modal" onClick={() => setModalOpen(false)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close" onClick={() => setModalOpen(false)}>&times;</span>
+            <Login onClose={() => setModalOpen(false)} />
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
