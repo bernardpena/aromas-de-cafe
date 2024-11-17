597e1bc33ce1c6d4518f5f53afec4b0b0c0a274b
Bernardo <bernardpena@gmail.com>
2024-11-17 19:56:40 -0300

    modificacion de base de datos

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -15,26 +15,13 @@ const Register = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     switch (name) {
-      case 'name':
-        setNombre(value);
-        break;
-      case 'email':
-        setEmail(value);
-        break;
-      case 'password':
-        setPassword(value);
-        break;
-      case 'calle':
-        setCalle(value);
-        break;
-      case 'ciudad':
-        setCiudad(value);
-        break;
-      case 'comuna':
-        setComuna(value);
-        break;
-      default:
-        break;
+      case 'name': setNombre(value); break;
+      case 'email': setEmail(value); break;
+      case 'password': setPassword(value); break;
+      case 'calle': setCalle(value); break;
+      case 'ciudad': setCiudad(value); break;
+      case 'comuna': setComuna(value); break;
+      default: break;
     }
   };
 
@@ -46,30 +33,19 @@ const Register = () => {
     try {
       const response = await fetch('http://localhost:5000/api/auth/register', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          nombre,
-          email,
-          password,
-          calle,
-          ciudad,
-          comuna,
-        }),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nombre, email, password, calle, ciudad, comuna }),
       });
 
       if (!response.ok) {
         const errorText = await response.text();
         console.error('Error en el registro:', errorText);
-
-        // Manejar el mensaje de error 
-        if (response.status === 409) { // 409 
+        if (response.status === 409) {
           setError('El email ya está en uso');
         } else {
           setError('Ocurrió un error al registrar el usuario');
         }
-        setModalOpen(true);
+        setModalOpen(true); // Mostrar el modal
       } else {
         const data = await response.json();
         console.log(data);
@@ -84,71 +60,25 @@ const Register = () => {
   return (
     <div className="container registro mt-5 my-5">
       <h2 className="text-center mb-2">Registro</h2>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setModalOpen(false)}
-        message={error}
-      /> {/* Componente Modal */}
+      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} message={error} />
       <form onSubmit={handleSubmit} className="p-4 rounded">
         <div className="mb-3">
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            placeholder="Nombre"
-            onChange={handleChange}
-            required
-          />
+          <input type="text" name="name" className="form-control" placeholder="Nombre" onChange={handleChange} required />
         </div>
         <div className="mb-3">
-          <input
-            type="email"
-            name="email"
-            className="form-control"
-            placeholder="Email"
-            onChange={handleChange}
-            required
-          />
+          <input type="email" name="email" className="form-control" placeholder="Email" onChange={handleChange} required />
         </div>
         <div className="mb-3">
-          <input
-            type="password"
-            name="password"
-            className="form-control"
-            placeholder="Contraseña"
-            onChange={handleChange}
-            required
-          />
+          <input type="password" name="password" className="form-control" placeholder="Contraseña" onChange={handleChange} required />
         </div>
         <div className="mb-3">
-          <input
-            type="text"
-            name="calle"
-            className="form-control"
-            placeholder="Calle"
-            onChange={handleChange}
-            required
-          />
+          <input type="text" name="calle" className="form-control" placeholder="Calle" onChange={handleChange} required />
         </div>
         <div className="mb-3">
-          <input
-            type="text"
-            name="ciudad"
-            className="form-control"
-            placeholder="Ciudad"
-            onChange={handleChange}
-            required
-          />
+          <input type="text" name="ciudad" className="form-control" placeholder="Ciudad" onChange={handleChange} required />
         </div>
         <div className="mb-3">
-          <input
-            type="text"
-            name="comuna"
-            className="form-control"
-            placeholder="Comuna"
-            onChange={handleChange}
-            required
-          />
+          <input type="text" name="comuna" className="form-control" placeholder="Comuna" onChange={handleChange} required />
         </div>
         <button type="submit" className="btn btn-primary w-100">Registrarse</button>
       </form>
