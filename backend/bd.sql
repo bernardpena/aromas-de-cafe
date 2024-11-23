40797d4129bc2eb3b2c24cdd763e06af868b49d2
Bernardo <bernardpena@gmail.com>
2024-11-23 13:42:55 -0300

    actualizacion de base de datos

--- backend/bd.sql
+++ backend/bd.sql
@@ -42,17 +42,6 @@ CREATE TABLE carrito (
 SELECT * FROM carrito;
 
 
---agregando administradores
-INSERT INTO usuarios (id, nombre, email, pass, calle, ciudad, comuna, rol) VALUES 
-('1', 'Anakaren', 'admin@gmail.com', 'Admin', 'jdksfhalkf', 'kdsfjhkljf', 'sdfssagf', 'Administrador');
-
-INSERT INTO usuarios (id, nombre, email, pass, calle, ciudad, comuna, rol) VALUES 
-('2', 'Bernardo', 'admin2@gmail.com', 'Admin2', 'Digo de Almagro', 'Calama', 'El Loa', 'Administrador');
-
-INSERT INTO usuarios (id, nombre, email, pass, calle, ciudad, comuna, rol) VALUES 
-('3', 'Lucas', 'admin3@gmail.com', 'Admin3', 'vcvcvcvcv', 'cvcvcvcv', 'cvcvcv', 'Administrador');
-
-
 INSERT INTO public.productos (id, nombre, descripcion, precio, imagen) VALUES
 (1, 'Café Arabica', 'Un café suave y aromático con notas de frutas.', 3.99, 'cafe1.jpg'),
 (2, 'Café Robusta', 'Café fuerte y con cuerpo, ideal para los amantes del café intenso.', 4.49, 'cafe2.jpg'),
@@ -67,20 +56,15 @@ SELECT * FROM productos;
 
 
 INSERT INTO public.usuarios (id, email, password, calle, ciudad, comuna, rol, nombre) VALUES
-(1, 'admin@gmail.com', 'Admin', 'jdksfhalkf', 'kdsfjhkljf', 'sdfssagf', 'Administrador', 'Anakaren'),
-(2, 'admin2@gmail.com', 'Admin2', 'Digo de Almagro', 'Calama', 'El Loa', 'Administrador', 'Bernardo'),
-(3, 'admin3@gmail.com', 'Admin3', 'vcvcvcvcv', 'cvcvcvcv', 'cvcvcv', 'Administrador', 'Lucas'),
-(5, 'marielaolivaresramos@gmail.com', '$2a$10$XcjINwqEa3vDKiiJtnfk8.ojozDIT6Ud4innaW0WkGQaVhTSTwU5C', 'Diego de Almagro 2382', 'Calama', 'Calama', NULL, 'Mariela Olivares'),
-(6, 'sebastianpenaolivares@gmail.com', '$2a$10$wNEzwyVTIMitChkXPuaq/eFwYSHqrC5gcVvkcDNAG6c6fcm0x.hPK', 'Diego de Almagro 2382', 'Calama', 'Calama', NULL, 'Sebastian Peña Olivares'),
-(7, 'marcelopenamolina@gmail.com', '$2a$10$R1QiyoI1Cnqr8PGxqR39K.PT0/MWNXuIgXh1xk3Vpju81jhHATgO6', 'Pacifico 2270', 'Calama', 'Calama', 'usuario', 'Marcelo Peña'),
-(8, 'marielaolivaresrubia@gmail.com', '$2a$10$/muF7SuhgPyaFyxB1Uftre..Vm3ZbhtygQk.g3e3T6W51g7fLMVTy', 'Diego de Almagro 2382', 'Calama', 'Calama', 'usuario', 'Mariela Olivares Ramos'),
-(9, 'yasnapena@gmail.com', '$2a$10$3sqY94pipsDpLVeujf9Bwu9bOmqNZ7xACyWyfmdd./j7lfCVnHx2m', 'calle', 'Calama', 'Calama', 'usuario', 'Yasna Peña'),
 (10, 'petete@gmail.com', '$2a$10$ZEGPFKCSxCClabYJohx0yO1VcoINmysvwifFJlc2JIfBymxoxcoYO', 'calama', 'calama', 'calama', 'usuario', 'Petete'),
 (11, 'mozita@gmail.com', '$2a$10$PjW.X498kvUhBE7XcVAW.u7GD9f/QfXpoQXIB1cAez7HdYeH.i/pa', 'calama', 'calama', 'calama', 'usuario', 'Mozita'),
 (12, 'leo@gmail.com', '$2a$10$7KQSBc5JAMYaGkTU1g32neyd4EdCq/ho/Mjz9rEvXvK8l7QsAqcWa', 'calama', 'calama', 'Calama', 'usuario', 'Leo'),
 (13, 'pedro@gmail.com', '$2a$10$LbpiKP2ZHgzO7Yv/n3DTeeVT.hfQ3uQO0EUKvOJvyi8H4XpyTNtLi', 'calama', 'Calama', 'Clama', 'usuario', 'Pedro'),
 (14, 'wilson@gmail.com', '$2a$10$vevoFSxLm.ubHnGR2fvN6uIgLGbGLPpP74u7ZwlPChgt12mLtnYwq', 'Calama', 'Calama', 'Calama', 'usuario', 'Wilson'),
-(15, 'roberto@gmail.com', '$2a$10$E/v7zueTyM/D0g33kJgQne2PMGDmbJIR/aheVoAzgSp1iGQ9D5YH.', 'Calama', 'Clamaa', 'Clama', 'usuario', 'Roberto');
+(15, 'roberto@gmail.com', '$2a$10$E/v7zueTyM/D0g33kJgQne2PMGDmbJIR/aheVoAzgSp1iGQ9D5YH.', 'Calama', 'Clamaa', 'Clama', 'usuario', 'Roberto'),
+(16, 'bernardo@aromasdecafe.cl', '$2a$10$zn6nNtSjKHGQLjQFCXaFq.cGNi7OJnFDLNGzNrO88aMN1z8I3NLOa', 'Diego de Almagro', NULL, 'Calama', 'admin', 'Bernardo'),
+(17, 'anakaren@aromasdecafe.cl', '$2a$10$RX2UjIhOuYY7sInTMdetbuw.EN3fsdX74JrXMxQgf5KKAkZRrjhRu', 'Central Sur', NULL, 'Mexico', 'admin', 'Anakaren Lozano'),
+(18, 'lucas@aromasdecafe.cl', '$2a$10$3zzp8ov5OHdVB56Sn/84T.6S1SzeSL3Wb4SKr0S9cbAC22apkSlce', 'Estacion Central', NULL, 'Estacion Central', 'admin', 'Lucas Cid');
 
 
 --tabla invitados
@@ -130,3 +114,14 @@ CREATE TABLE compras (
     valor DECIMAL(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO compras (usuario_id, invitado_id, producto_id, cantidad, email, descripcion, imagen, nombre, valor, created_at) VALUES
+(13, NULL, 6, 1, NULL, 'Café suave con leche espumosa, un clásico en cualquier cafetería.', 'cafe6.jpg', 'Café Latte', 4.29, '2024-11-23 10:53:20'),
+(13, NULL, 6, 1, NULL, 'Café suave con leche espumosa, un clásico en cualquier cafetería.', 'cafe6.jpg', 'Café Latte', 4.29, '2024-11-23 11:03:46'),
+(13, NULL, 1, 1, NULL, 'Un café suave y aromático con notas de frutas.', 'cafe1.jpg', 'Café Arabica', 3.99, '2024-11-23 12:07:07'),
+(13, NULL, 1, 1, NULL, 'Un café suave y aromático con notas de frutas.', 'cafe1.jpg', 'Café Arabica', 3.99, '2024-11-23 12:13:14'),
+(13, NULL, 2, 1, NULL, 'Café fuerte y con cuerpo, ideal para los amantes del café intenso.', 'cafe2.jpg', 'Café Robusta', 4.49, '2024-11-23 12:15:58'),
+(13, NULL, 3, 1, NULL, 'Café de origen único, seleccionado por su calidad excepcional.', 'cafe3.jpg', 'Café de Especialidad', 5.99, '2024-11-23 12:23:12'),
+(13, NULL, 3, 1, NULL, 'Café de origen único, seleccionado por su calidad excepcional.', 'cafe3.jpg', 'Café de Especialidad', 5.99, '2024-11-23 12:27:37'),
+(13, NULL, 1, 1, NULL, 'Un café suave y aromático con notas de frutas.', 'cafe1.jpg', 'Café Arabica', 3.99, '2024-11-23 12:29:41'),
+(13, NULL, 4, 1, NULL, 'Café sin cafeína, perfecto para disfrutar en cualquier momento del día.', 'cafe4.jpg', 'Café Descafeinado', 3.49, '2024-11-23 12:38:23');
