0e13202a0e8db6c318c2c153cc95760a9a91081e
Bernardo <bernardpena@gmail.com>
2024-12-01 15:53:39 -0300

    modificacion de index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,43 @@
-// Anakaren
-
-const jwt = require("jsonwebtoken");
-
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const { getEventos, deleteEvento } = require("./consultas");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const salesRoutes = require("./routes/salesRoutes");
+const db = require("./config/db");
 
-app.listen(5001, console.log("SERVER ON"));
-app.use(cors());
+const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/eventos", async (req, res) => {
-  try {
-    const eventos = await getEventos();
-    res.json(eventos);
-  } catch (error) {
-    res.status(error.code || 500).send(error);
+// Rutas
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api/sales", salesRoutes);
+
+// error 404
+app.use((req, res) => {
+  res.status(404).json({ error: "Ruta no encontrada" });
+});
+
+// Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Algo salió mal!");
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error al conectar a la base de datos:", err);
+    process.exit(1);
   }
+  console.log("Conexión a la base de datos establecida correctamente.");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
