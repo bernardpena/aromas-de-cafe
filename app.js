0d80097f7d9f2c8a341a1e5d9fe3e31cbfbb4fda
Bernardo <bernardpena@gmail.com>
2024-12-01 19:29:43 -0300

    modificacion de cors

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(bodyParser.json());
 
+//para conectarse con el frontend
 app.use(
   cors({
     origin: "https://aromas-de-cafe.onrender.com/",
