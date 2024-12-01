113368d2cada29efa63b758e2c3195f4f0ec961f
Bernardo <bernardpena@gmail.com>
2024-12-01 17:26:00 -0300

    modificacion conexiones

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,22 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'aromas_de_cafe',
+//   password: 'Admin',
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'aromas_de_cafe',
-  password: 'Admin',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 
