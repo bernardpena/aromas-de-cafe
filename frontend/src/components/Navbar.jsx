b6cc880245ad482da282ea051d9bacc6775338e0
Bernardo <bernardpena@gmail.com>
2024-12-01 15:35:10 -0300

    Modificacion de logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar({ onLoginClick }) {
     <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img className="logo" src="../public/AromasdeCafe_LOGO.png" alt="Aromas de Café Logo" />
+          <img className="logo" src="./public/AromasdeCafe_LOGO.png" alt="Aromas de Café Logo" />
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
