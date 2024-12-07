12ca9e59b102b28f0903b2b74219c77235727c7a
Bernardo <bernardpena@gmail.com>
2024-12-07 17:37:11 -0300

    agregando Middleware

---
+++ backend/routes/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ message: "No se proporcionó un token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; 
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token inválido" });
+  }
+};
+
+module.exports = { authenticate };
