651ca1675c1643de3757416186ef32586ad691b1
Bernardo <bernardpena@gmail.com>
2024-12-07 23:51:37 -0300

    modificacion para agregar productos2

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const pool = require("../config/db");
+const Product = require("../models/Product");
 
 exports.getProducts = async (req, res) => {
   try {
@@ -7,5 +8,24 @@ exports.getProducts = async (req, res) => {
   } catch (err) {
     console.error("Error al obtener los productos:", err);
     res.status(500).json({ error: "Error al obtener los productos" });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { activo } = req.body; // Por ejemplo, el campo que se está actualizando
+
+  try {
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { activo },
+      { new: true }
+    );
+    if (!product)
+      return res.status(404).json({ message: "Producto no encontrado" });
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Error al actualizar producto" });
   }
 };
