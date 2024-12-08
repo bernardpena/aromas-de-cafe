38aee2b80f16d354d427d5be054643429782d2bd
Bernardo <bernardpena@gmail.com>
2024-12-08 11:46:46 -0300

    modificacion para agregar productos25

--- frontend/src/components/AdminProductManagement.jsx
+++ frontend/src/components/AdminProductManagement.jsx
@@ -28,7 +28,7 @@ function AdminProductManagement() {
     const handleToggleProductStatus = async (productId, currentStatus) => {
         try {
             // const response = await fetch(`https://backend-585p.onrender.com/api/products/${productId}`, {
-            const response = await fetch(`https://backend-585p.onrender.com/api/products/${Id}`, {
+            const response = await fetch(`https://backend-585p.onrender.com/api/products/${id}`, {
                 method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json',
