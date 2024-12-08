b0f3c1b057f64c3789bc9b1a05f6acf0f28836e1
Bernardo <bernardpena@gmail.com>
2024-12-08 13:59:53 -0300

    modificacion para agregar productos33 arreglando errores

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,26 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const productController = require("../controllers/productController");
 const { authenticate } = require("../middleware/authMiddleware"); // Middleware
 
 // Ruta registrar un usuario
-router.post("/register", (req, res, next) => {
-    console.log("Solicitud de registro recibida");
-    next();
-}, authController.register);
+router.post(
+    "/register",
+    (req, res, next) => {
+      console.log("Solicitud de registro recibida");
+      next();
+    },
+    authController.register
+  );
+  
+  // Ruta sesión
+  router.post("/login", authController.login);
+  
+  // Ruta para obtener los datos del perfil del usuario
+  router.get("/profiles", authenticate, authController.getProfile);
+  
+  // Ruta para actualizar los datos del usuario
+  router.put("/update", authenticate, authController.updateProfile);
+  
+  // Ruta administrador
+  router.post("/add-admin", authController.addAdministrator);
+  
+  // Rutas para productos
+  router.get("/products", authenticate, productController.getProducts);
 
-
-// Ruta sesión
-router.post("/login", authController.login);
-
-// Ruta para obtener los datos del perfil del usuario
-router.get("/profiles", authenticate, authController.getProfile);
-
-// Ruta para actualizar los datos del usuario
-router.put("/update", authenticate, authController.updateProfile);
-
-// Ruta administrador
-router.post("/add-admin", authController.addAdministrator);
-
+  router.patch("/products/:id", productController.updateProduct);
 
 module.exports = router;
